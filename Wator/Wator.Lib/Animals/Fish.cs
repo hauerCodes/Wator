dcce6b5971ae7f0d4e1580bce94b3e39d5abf65c
Christoph Hauer <hauer.christoph@live.at>
2015-06-20 17:01:03 +0000

    Initial Checkin

---
+++ Wator/Wator.Lib/Animals/Fish.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wator.Lib.Animals
+{
+    public class Fish : Animal
+    {
+        public override void Step()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Finish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Ageing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wator/Wator.Lib/World/WatorSettings.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wator.Lib.World
+{
+    public class WatorSettings
+    {
+        public int WorldHeight
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int WorldWidth
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int InitialFishPopulation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int InitialSharkPopulation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int SharkBreedTime
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int FishBreedTime
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int SharkStarveTime
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
